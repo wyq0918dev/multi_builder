bbcc20dcf75742008f4f2304542b9af57ececad2
wyq0918dev <wyq0918dev@gmail.com>
2026-05-05 16:35:49 +0800

    注释

--- lib/multi_builder_web.dart
+++ lib/multi_builder_web.dart
@@ -8,6 +8,9 @@ import 'multi_builder.dart';
 final class MultiBuilderWeb {
   MultiBuilderWeb._();
 
+  /// 平台插件注册
+  ///
+  /// 插件注册由Flutter框架接管, 请勿手动注册.
   static void registerWith(Registrar _) {
     MultiBuilderInterface.instance = MultiBuilderImpl();
   }
